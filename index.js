f7dcb800afa8cd3653178b6e136445e2c0fce58a
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-06-16 12:11:18 -0300

    [SPEE-5] Make a library with it (#4)
    ## [SPEE-5] Description
    Creating a NPM library with the project.

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const SerialPeer = require('./src/SerialPeer');
-const drive = require('./driver');
+const SerialEndpoint = require('./src/SerialEndpoint');
 
-global.serialPort = new SerialPeer({
-   baudRate: 1000000,
-   endpoints: [ drive ],
-   onOpen() {
-      console.log('Serial opened!');
-   },
-   onData(data) {
-      console.log(data);
-   },
-   onError(err) {
-      console.log(err);
-      debugger;
-   }
-});
+module.exports = {
+   SerialPeer,
+   SerialEndpoint
+};
