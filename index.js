b79632b8f1dd08bc7c06b54fab0ecab33841ed5f
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-06-16 10:59:49 -0300

    [SPEE-2] Create the basic of main class (#2)
    ## [SPEE-2] Description
    Creating the SerialPeer class instance

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-const { SerialPort } = require('serialport');
-const { ReadlineParser } = require('@serialport/parser-readline');
+const SerialPeer = require('./src/SerialPeer');
 
-// Adjust the serial port path as needed
-const port = new SerialPort({
-   baudRate: 9600,
-   path: '/dev/ttyS0'
-});
-
-const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
-
-port.on('open', () => {
-   console.log('Serial Port Opened');
-});
-
-parser.on('data', data => {
-   if (data) {
-      console.log('Received:', data);
+const serial = new SerialPeer({
+   onOpen() {
+      console.log('Serial opened!');
+   },
+   onData(data) {
+      console.log('Received: ', data);
+   },
+   onError(err) {
+      debugger;
    }
-});
-
-parser.write('Hello There!', err => {
-   if (err) throw err;
-});
-
-port.on('error', err => {
-   console.error('Error: ', err.message);
 });
